2c5749806290c393fb6eee2c6693f84ee6b23b42
Mori116 <cacaomame096@gmail.com>
2021-05-07 07:00:12 +0000

    [Add]投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,28 @@
 class BooksController < ApplicationController
   def index
   end
+
+  def show
+  end
+
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/index'
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def edit
+  end
+
 end
